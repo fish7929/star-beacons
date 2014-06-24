4bf98b0fcb7973d1fae2b4f74698cd2d3766810c
YU Jin <yj@183d.scs.im>
2014-06-24 01:56:42 +0000

    rm custon action

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -91,10 +91,6 @@ class TemplatesController < ApplicationController
   
   end
 
-	def custom
-
-	end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_template
